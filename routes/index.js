ab3a2f8457a38e1c38504e55ae14e8c99e991879
Daniel Martín García <danielmartingarcia@outlook.es>
2020-06-01 21:11:19 +0200

    Login: first version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,90 @@
 var express = require('express');
 var router = express.Router();
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+
+// To support URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  // Get auth token from the cookies
+  const authToken = req.cookies['AuthToken'];
+
+  // Inject the user to the request
+  req.user = authTokens[authToken];
+
+  next();
+});
+
+
+//AUX
+const getHashedPassword = (password) => {
+  const sha256 = crypto.createHash('sha256');
+  const hash = sha256.update(password).digest('base64');
+  return hash;
+}
+const authTokens = {};
+
+const generateAuthToken = () => {
+  return crypto.randomBytes(30).toString('hex');
+}
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+
+/* GET login page. */
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+/* POST login form. */
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = getHashedPassword(password);
+
+  const user = users.find(u => {
+    return u.email === email && hashedPassword === u.password
+  });
+
+  if (user) {
+    const authToken = generateAuthToken();
+
+    // Store authentication token
+    authTokens[authToken] = user;
+
+    // Setting the auth token in cookies
+    res.cookie('AuthToken', authToken);
+
+    // Redirect user to the protected page
+    res.redirect('/protected');
+  } else {
+    res.render('login', {
+      message: 'Invalid username or password',
+      messageClass: 'alert-danger'
+    });
+  }
+});
+
+app.get('/protected', (req, res) => {
+  if (req.user) {
+    res.render('protected');
+  } else {
+    res.render('login', {
+      message: 'Please login to continue',
+      messageClass: 'alert-danger'
+    });
+  }
 });
 
 module.exports = router;
